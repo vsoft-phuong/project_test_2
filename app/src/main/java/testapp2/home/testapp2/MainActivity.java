f8e318b3f3771a504fa98aac3732a5ae6b77a07c
Pham Trung Phuong <vsoftphuong@gmail.com>
2015-04-20 16:44:34 +0700

    Cap nhat MainActivity

--- app/src/main/java/testapp2/home/testapp2/MainActivity.java
+++ app/src/main/java/testapp2/home/testapp2/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -25,6 +26,7 @@ public class MainActivity extends ActionBarActivity
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(mToolbar);
+        Log.e("tag", "test");
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.fragment_drawer);
